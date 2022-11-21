a3db2c7e018a7a5f6ea3e1e1ac63fd44d0e09b57
JULIAN888S <38394306+JULIAN888S@users.noreply.github.com>
2022-11-21 16:45:59 -0500

    Add files via upload

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,8 +7,20 @@ function Welcome (props) {
     return(
         <div  className="main-container">
         <div className="text-block" >  
-        <h2>Nosotros somos Horus Web Colombia!</h2>
-        <p>Ofrecemos las mejores opciones de desarrollo web y diseño grafico para su empresa y emprendimiento</p>
+        <span></span>
+        <span></span>
+        <h2>¡NOSOTROS SOMOS HORUS WEB COLOMBIA! </h2>
+        <p>Somos una empresa Colombiana que se enfoca en brindar soluciones y 
+            servicios de sitios web a las pequeñas y medianas empresas con el 
+            fin de generar mayor visibilidad comercial para poder llegar a nuevos 
+            clientes y generar mayores ingresos. <br/>
+
+            Nos encantaría que conocieras nuestro catálogo de servicios, 
+            que nos de la oportunidad de hacer más visible tu negocio,
+            no dudes en contactarnos.
+            <span></span>
+            <br/>
+            <strong>¡Bienvenido!</strong></p>
         </div>  
         <img src={image} alt="Welcome" width="500" height="600"/>
         
